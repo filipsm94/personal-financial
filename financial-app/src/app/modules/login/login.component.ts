09b8c1b51d007416dc28eb9a5ab45474ede3a5b9
Filipsm94 <filipsm94@gmail.com>
2021-05-15 11:47:35 -0500

    config main page, and login page style

--- financial-app/src/app/modules/login/login.component.ts
+++ financial-app/src/app/modules/login/login.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  host: {'class': 'ds-fx-ct'}
 })
 export class LoginComponent implements OnInit {
 
