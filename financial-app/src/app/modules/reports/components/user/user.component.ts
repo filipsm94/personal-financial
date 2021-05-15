a66e0594e7d97e26522049c61a2d0a9302140f54
Filipsm94 <filipsm94@gmail.com>
2021-05-15 15:03:16 -0500

    add form user and config login too

--- financial-app/src/app/modules/reports/components/user/user.component.ts
+++ financial-app/src/app/modules/reports/components/user/user.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
+  host: {'class': 'ds-fx-ct'}
 })
 export class UserComponent implements OnInit {
+  public userInfoForm: FormGroup;
+  private infoLogin: any;
 
-  constructor() { }
+  public hasError = false;
+
+  constructor() {
+    this.userInfoForm = new FormGroup({
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(4)]),
+      correo: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(4)]),
+      celular: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(4)]),
+      genero: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(4)]),
+    });
+  }
+
+  get name(): any { return this.userInfoForm.get('name'); }
+  get correo(): any { return this.userInfoForm.get('correo'); }
+  get celular(): any { return this.userInfoForm.get('celular'); }
+  get genero(): any { return this.userInfoForm.get('genero'); }
 
   ngOnInit(): void {
+  }
+
+  async sendInfoUser(){
+    // await this.userService.setUserInfo({...this.loginForm.value});
   }
 
 }
