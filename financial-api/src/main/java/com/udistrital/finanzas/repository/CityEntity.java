964fef420025bd07cafc11fcf6c769f632708c5d
jhogarcia <jhonattan.garcia@mercadolibre.com.co>
2021-05-21 21:42:14 -0500

    initial code example spring boot rest and database

---
+++ financial-api/src/main/java/com/udistrital/finanzas/repository/CityEntity.java
@@ -0,0 +1,20 @@
+package com.udistrital.finanzas.repository;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class CityEntity {
+    @Id
+    int id;
+    @Column
+    String name;
+    @Column
+    String province;
+}
---
+++ financial-api/src/main/java/com/udistrital/finanzas/repository/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.udistrital.finanzas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository  extends JpaRepository<CustomerEntity, Long> {
+}
